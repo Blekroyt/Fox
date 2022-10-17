be3ec90da6344211f05fe17779ad8c2dc4be23cd
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-10-17 18:19:20 +0300

    Update Индивид.py

--- Индивид.py
+++ Индивид.py
@@ -9,8 +9,8 @@ def fun1(type_='max'):
     return fun2
    
 if __name__ == '__main__':
- a = [1, 2, 3, 4, 5, 65, 6,]
- max_fun = fun1()
- min_fun = fun1('min')
- print(max_fun(a))
- print(min_fun(a))
+    a = [1, 2, 3, 4, 5, 65, 6,]
+    max_fun = fun1()
+    min_fun = fun1('min')
+    print(max_fun(a))
+    print(min_fun(a))
